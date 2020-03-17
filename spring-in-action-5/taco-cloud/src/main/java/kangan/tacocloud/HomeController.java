d8c8677fd04cd8c50ff3b4c809b27ac9f3294bf5
rflbc <56537351+rflbc@users.noreply.github.com>
2020-03-17 21:58:23 +0800

    upload project

---
+++ spring-in-action-5/taco-cloud/src/main/java/kangan/tacocloud/HomeController.java
@@ -0,0 +1,13 @@
+package kangan.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public String getHome() {
+		return "home";
+	}
+}
---
+++ spring-in-action-5/taco-cloud/src/test/java/kangan/tacocloud/HomeControllerTest.java
@@ -0,0 +1,29 @@
+package kangan.tacocloud;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+class HomeControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testGetHome() throws Exception {
+		mockMvc.perform(get("/"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("home"))
+		.andExpect(content().string(containsString("Welcome to Taco Cloud")));
+	}
+
+}
