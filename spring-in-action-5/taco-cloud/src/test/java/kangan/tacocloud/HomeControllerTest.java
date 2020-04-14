c09a91850440cfda22ff8087be46c4407f67ade4
rflbc <56537351+rflbc@users.noreply.github.com>
2020-04-14 13:27:58 +0800

    no message

--- spring-in-action-5/taco-cloud/src/test/java/kangan/tacocloud/HomeControllerTest.java
+++ spring-in-action-5/taco-cloud/src/test/java/kangan/tacocloud/HomeControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import kangan.tacocloud.HomeController;
 
 @WebMvcTest(HomeController.class)
 class HomeControllerTest {
